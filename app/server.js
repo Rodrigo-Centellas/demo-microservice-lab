dc1af78a11609cd550a96c6e2903a03559d4a04e
Rodrigo Centellas <101054195+Rodrigo-Centellas@users.noreply.github.com>
2025-10-31 17:36:40 -0400

    segundo intento

--- app/server.js
+++ app/server.js
@@ -1,15 +1,16 @@
 import app from "./app.js";
 import { PORT } from './config.js';
-import userRouter from './routes/users.routes.js';
+import userRouter from './routes/users.routes.js'; 
+import express from "express"; 
+
+
+
+app.use(express.json()); // Middleware aplicado
+app.use(userRouter);     // Rutas aplicadas
 
 app.listen(PORT);
-app.use(express.json());
-app.use(userRouter);
 console.log("server on port", PORT)
-
 
 
 process.on("SIGTERM", () => server.close(() => process.exit(0)));
 process.on("SIGINT", () => server.close(() => process.exit(0)));
-
-
